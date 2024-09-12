3a8fcb7b2e1b40a2732b7fc7d780b83630fe5a7e
samgbsr <sam.garcia.96@gmail.com>
2024-09-12 17:25:12 -0600

    Normalize line endings

--- app/models/prediction_result.rb
+++
@@ -1,2 +0,0 @@
-class PredictionResult < ApplicationRecord
-end
--- db/migrate/20240912185502_create_prediction_results.rb
+++
@@ -1,9 +0,0 @@
-class CreatePredictionResults < ActiveRecord::Migration[7.2]
-  def change
-    create_table :prediction_results do |t|
-      t.text :probabilidades
-      t.string :clase_predicha
-      t.timestamps
-    end
-  end
-end
